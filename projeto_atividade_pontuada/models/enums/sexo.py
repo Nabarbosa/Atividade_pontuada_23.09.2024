7b120693e875d875c34a24146618bb187d137177
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-10-03 11:07:23 +0000

    update

--- projeto_atividade_pontuada/models/enums/sexo.py
+++ projeto_atividade_pontuada/models/enums/sexo.py
@@ -4,6 +4,6 @@ class Sexo(Enum):
     MASCULINO = ("Masculino", "M")
     FEMININO = ("Feminino", "F")
 
-    def __init__(self, texto_sexo: str, caracter: str) -> None:
+    def __init__(self, texto_sexo: str, caracter_sexo: str) -> None:
         self.texto = texto_sexo
-        self.caracter = caracter
+        self.caracter = caracter_sexo
