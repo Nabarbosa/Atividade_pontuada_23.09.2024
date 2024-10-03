ec70396b8069155fd07a0ed1b81998a8aafb3b13
Annfranco <160090372+Annfranco@users.noreply.github.com>
2024-10-03 11:00:58 +0000

    update

--- projeto_atividade_pontuada/models/enums/estado_civil.py
+++ projeto_atividade_pontuada/models/enums/estado_civil.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class Estado_civil(Enum):
-    SOLTEIRO = "Solteira"
+    SOLTEIRO = "Solteiro"
     CASADO = "Casado"
     SEPARADO = "Separado"
     DIVORCIADO = "Divorciado"
     VIUVO = "Viuvo"
 
-    def __init__(self, texto:str) -> None:
-        self.texto = texto
+    def __init__(self, texto_estadoCivil:str) -> None:
+        self.texto = texto_estadoCivil
